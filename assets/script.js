f53fefb09d91a5caa0631b7e74120549a8458c03
Nate Traugh <n.traugh@yahoo.com>
2022-05-23 17:21:46 -0400

    finished the if statement to find out number of characters selected

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 // Assignment code
-var generateBtn = document.getElementById("generate");
+var generateBtn = document.querySelector("#generate");
 // Instructor provided
 function generatePassword(){
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
@@ -10,11 +10,15 @@ function generatePassword(){
   var input = parseInt(prompt("Choose a number between 8 and 128 characters.")) // parseINT turns the answer into an integer (full number)
 
   if(input >= 8 && input <= 128){
-    // need code in here
-
+    alert("Your password will be " + input + " characters long") // validating the users choice
   }else{
     alert("Pick a number between 8 and 128")
+    return generatePassword(); // re-runs the function so the user can put in a number between the prompted characters
   }
+
+   
+
+  
 
   return ""; // Placeholder
 }
@@ -30,3 +34,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+generatePassword();
