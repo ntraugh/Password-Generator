dba6793d3dfc66139c522cd058ec1c41ab5705de
Nate Traugh <n.traugh@yahoo.com>
2022-05-23 18:01:49 -0400

    added if else statements for all the conditions (upper,lower, num, special)

--- assets/script.js
+++ assets/script.js
@@ -8,13 +8,28 @@ function generatePassword(){
   var characters = "#$%&()*+,-./:;<=>?"
 
   var input = parseInt(prompt("Choose a number between 8 and 128 characters.")) // parseINT turns the answer into an integer (full number)
-
+  
   if(input >= 8 && input <= 128){
-    alert("Your password will be " + input + " characters long") // validating the users choice
-  }else{
+    alert("Your password will be " + input + " characters long"); // validating the users choice
+  }
+  else{
     alert("Pick a number between 8 and 128")
     return generatePassword(); // re-runs the function so the user can put in a number between the prompted characters
   }
+
+  var lower = prompt("Would you like lowercase letters?")
+  var choiceLower = lower.toLowerCase(); // converted choice to lowercase, if answer is not YES, Yes, or yes, the else statement will go into effect.
+  console.log(choiceLower)
+  if(choiceLower === "yes"){
+    alert("We will add lowercase letters to your password.");
+
+  }else{
+    alert("No lowercase letters will be added.");
+
+  }
+
+
+
 
    
 
