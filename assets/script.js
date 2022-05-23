fb489dca62675443d8745e1e877fd74f9def2967
Nate Traugh <n.traugh@yahoo.com>
2022-05-23 12:30:46 -0400

    started the code, defined our variabes

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,29 @@
-// Assignment Code
-var generateBtn = document.getElementById("#generate");
+// Assignment code
+var generateBtn = document.getElementById("generate");
+// Instructor provided
+function generatePassword(){
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "1234567890";
+  var characters = "#$%&()*+,-./:;<=>?"
+
+  var input = parseInt(prompt("Choose a number between 8 and 128 characters.")) // parseINT turns the answer into an integer (full number)
+
+  if(input >= 8 && input <= 128){
+    // need code in here
+
+  }else{
+    alert("Pick a number between 8 and 128")
+  }
+
+  return ""; // Placeholder
+}
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = '';
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
