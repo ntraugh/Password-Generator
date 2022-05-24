9b7df86eabd2c22fb9572d71c3853bc801c6cea0
Nate Traugh <n.traugh@yahoo.com>
2022-05-23 22:06:10 -0400

    added a for loop to iterate through a chosen variable, getting undefined in the browser

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,9 @@ function generatePassword(){
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbers = "1234567890";
-  var characters = "#$%&()*+,-./:;<=>?"
+  var characters = "#$%&()*+,-./:;<=>?" 
+  
+
   var input = parseInt(prompt("Choose a number between 8 and 128 characters.")) // parseINT turns the answer into an integer (full number)
   
   if(input >= 8 && input <= 128){
@@ -18,7 +20,9 @@ function generatePassword(){
     return generatePassword(); // re-runs the function so the user can put in a number between the prompted characters
   }
 
-  var lower = prompt("Would you like lowercase letters? Yes or No") // if else for lowercase
+
+  // if else for lowercase
+  var lower = prompt("Would you like lowercase letters? Yes or No") 
   var choiceLower = lower.toLowerCase(); // converted choice to lowercase, if answer is not YES, Yes, or yes, the else statement will go into effect.
   console.log(choiceLower)
   if(choiceLower === "yes"){
@@ -30,7 +34,8 @@ function generatePassword(){
   }
  
 
-  var upper = prompt("Would you like uppercase letters? Yes or No")  // if else for uppercase
+  // if else for uppercase
+  var upper = prompt("Would you like uppercase letters? Yes or No")  
   var choiceUpper = upper.toLowerCase(); // converted choice to lowercase, if answer is not YES, Yes, or yes, the else statement will go into effect.
   console.log(choiceUpper)
   if(choiceUpper === "yes"){
@@ -41,7 +46,9 @@ function generatePassword(){
     choiceUpper = false;
   }
 
-  var number = prompt("Would you like numbers? Yes or No") // if else for numbers
+
+  // if else for numbers
+  var number = prompt("Would you like numbers? Yes or No") 
   var choiceNumber = number.toLowerCase(); // converted choice to lowercase, if answer is not YES, Yes, or yes, the else statement will go into effect.
   console.log(choiceNumber)
   if(choiceNumber === "yes"){
@@ -52,7 +59,9 @@ function generatePassword(){
     choiceNumber = false;
   }
 
-  var special = prompt("Would you like special symbols? Yes or No") // if else for special characters
+
+  // if else for special characters
+  var special = prompt("Would you like special symbols? Yes or No") 
   var choiceSpecial = special.toLowerCase(); // converted choice to lowercase, if answer is not YES, Yes, or yes, the else statement will go into effect.
   console.log(choiceSpecial)
   if(choiceSpecial === "yes"){
@@ -62,25 +71,36 @@ function generatePassword(){
     alert("No special characters will be added.");
     choiceSpecial = false
   }
+  
 
-  console.log(choiceLower)
+  var pass = "";
 
   // comparing our values to see which data types to add to the password
-
-
+  if(!choiceSpecial && !choiceNumber && !choiceUpper && !choiceLower){
+    alert("Must contain at least one type of character"); // promting the user they need to choose at least one of the options for characters
+    return generatePassword();
+  }
+  if(choiceSpecial && choiceNumber && choiceUpper && choiceLower){ // if all choices are picked
+    for (var i = 0; i < input; i++) {
+      var randPass = Math.floor(Math.random() * characters.length);
+      pass += characters.charAt(randPass)
+     
+    }
+  }
+  
 
 
   
   
 
-  return ""; // Placeholder
+ 
 }
 
 
 // Write password to the #password input
 function writePassword() {
-  generatePassword() // when the button is clicked we run the generate password function
-  var password = '';
+   // when the button is clicked we run the generate password function
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
   
@@ -89,4 +109,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
